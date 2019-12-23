302a0c2158036659b19c63a5a11d5a6ee6e8c0ce
刘斐笑 <390519764@qq.com>
2019-12-23 17:33:08 +0800

    feature: 生成网站元数据功能

---
+++ src/plugins/siteData.js
@@ -0,0 +1,9 @@
+module.exports = () => {
+    let files = fs.readdirSync(targetPath)
+    let siteData
+
+    routes.push({
+        path: path.join("/",dealName(value)),
+        component: dealName(value)
+    })
+}
