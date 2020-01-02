6fd969ba012c8c6151da0fda16f88fbe10895f90
刘斐笑 <390519764@qq.com>
2020-01-02 11:14:57 +0800

    chore: 添加axios

--- src/components/Admin/Home.vue
+++ src/components/Admin/Home.vue
@@ -1,3 +1,43 @@
 <template>
-    
+    <div class="adminHome">
+        <el-card>
+            commitNum: {{ commitNum }}
+        </el-card>
+        <el-card>
+            lastCommit: {{ lastCommit }}
+        </el-card>
+        <el-card>
+            commitNum: {{ commitNum }}
+        </el-card>
+    </div>
 </template>
+<script>
+export default {
+    data() {
+        return {
+            commitNum: 0,
+            lastCommit: 0
+        }
+    },
+    created() {
+        this.$axios
+            .get('https://api.github.com/repos/hahaaha/inteWeb/commits?sha=dev')
+            .then((data) => {
+                console.log(data.data)
+                this.commitNum = data.data.length
+                let d = new Date(data.data[0].commit.author.date)
+                this.lastCommit = `${d.getFullYear()}年${d.getMonth()+1}月${d.getDate()}日`
+            })
+    }
+}
+</script>
+<style>
+.adminHome {
+    display: flex;
+    justify-content: space-around;
+}
+.adminHome > .el-card {
+    width: 30%;
+    height: 80px;
+}
+</style>
--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import App from './App.vue'
 import router from './config/routers'
 import siteData from './config/mixin'
 import echarts from 'echarts'
+import axios from 'axios'
 
 Vue.config.productionTip = false
 Vue.mixin(siteData)
 Vue.prototype.$echarts = echarts
+Vue.prototype.$axios = axios
 
 new Vue({
     el: '#app',
