e233655af5114d953aa96327ca11f29537721fae
haha <390519764@qq.com>
2020-01-11 13:16:19 +0800

    fix: 修复vue-fullpage报错
    修复由default-passive-events造成的vue-fullpage报错

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import router from './config/routers'
 import siteData from './config/mixin'
 import echarts from 'echarts'
 import axios from 'axios'
-import 'default-passive-events'
 
 Vue.config.productionTip = false
 Vue.mixin(siteData)
